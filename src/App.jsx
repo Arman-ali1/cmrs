beef96d316169931f77dc37c196c091022a66a98
Arman Ali <armnsrn173200@gmail.com>
2025-02-24 19:16:00 +0530

    bwjnd

--- src/App.jsx
+++ src/App.jsx
@@ -20,7 +20,7 @@ import Cookies from "js-cookie";
 import { jwtDecode } from "jwt-decode";
 
 import Pagelayout1 from "./components/UserProfile/chat/UserChatBox.jsx";
-import Pagelayout2 from "./components/adminsidetrads/adminsidetradslist/chat/AdminChat.jsx";
+import Pagelayout2 from "./components/adminsidetrads/adminsidetradslist/chat/AdminChatBox.jsx";
 import HomeContainer from "./components/home/HomeContainer.js";
 import LayoutDashboardContainer from "./LayoutDashboardContainer.js";
 import LayoutUserContainer from "./LayoutUserContainer.js";
